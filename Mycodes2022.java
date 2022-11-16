197b75f85e19520ba6e3ae172a3a17bc2314f3c1
kamusiimeronah <117361888+kamusiimeronah@users.noreply.github.com>
2022-11-16 09:10:38 -0800

    Add files via upload

---
+++ Mycodes2022.java
@@ -0,0 +1,35 @@
+ package Ronah5;
+
+import java.util.Scanner;
+
+public class Mycodes2022 {
+
+	public static void main(String[] args) {
+	
+
+
+			//program to get student details
+		
+			
+			 Scanner sc = new Scanner(System.in);
+			 
+			          String name;
+			          String registration_number;
+			          
+			          int marks ;
+			          System.out.print("Enter Name: ");
+			          name=sc.nextLine();
+			          System.out.print("Enter registration number: ");
+			          registration_number = sc.nextLine();
+			          System.out.println("Enter marks for:");
+			          System.out.println("Linear programming:");
+			          marks = sc.nextInt();
+			          System.out.println("Object Oriented Programming:");
+			          marks = sc.nextInt();
+			          System.out.println("data structures and algorithm:");
+			          marks = sc.nextInt();
+			          System.out.println("scripting languages:");
+			          marks = sc.nextInt();
+			         
+			       
+			      }}
